b7794e331686266cf2597feeac7bd35f7469aa42
Leonardo Quevedo <leonardoq@trin.ca>
2020-10-09 17:08:51 -0300

    Updates project implementation.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,60 +1,63 @@
-const report = require('gatsby-cli/lib/reporter');
 const firebase = require('firebase');
 const crypto = require('crypto');
 
-const getDigest = (id) => crypto.createHash('md5').update(id).digest('hex');
+const getDigest = id =>
+  crypto
+    .createHash('md5')
+    .update(id)
+    .digest('hex');
 
 exports.sourceNodes = async (
   { boundActionCreators },
-  { types, credential, appConfig }
+  { types = [], credential, appConfig },
+  callback
 ) => {
   try {
     if (firebase.apps || !firebase.apps.length) {
-      const cfg = appConfig
-        ? appConfig
-        : { credential: firebase.credential.cert(credential) };
-      firebase.initializeApp(cfg);
+      const config = appConfig || {
+        credential: firebase.credential.cert(credential),
+      };
+      firebase.initializeApp(config);
+    }
+
+    const db = firebase.firestore();
+    const { createNode } = boundActionCreators;
+
+    for (let i = 0; i < types.length; i++) {
+      const entry = types[i];
+      if (entry) {
+        const { collection = '', type = '', map = node => node } = entry;
+        const snapshot = await db.collection(collection).get();
+
+        createNode({
+          id: '0',
+          parent: null,
+          children: [],
+          internal: {
+            type,
+            contentDigest: getDigest(`test-${collection}`),
+          },
+        });
+
+        for (let doc of snapshot.docs) {
+          const contentDigest = getDigest(doc.id);
+          createNode(
+            Object.assign({}, map(doc.data()), {
+              id: doc.id,
+              parent: null,
+              children: [],
+              internal: {
+                type,
+                contentDigest,
+              },
+            })
+          );
+        }
+      }
     }
   } catch (e) {
-    report.warn(
-      'Could not initialize Firebase. Please check `credential` property in gatsby-config.js'
-    );
-    report.warn(e);
-    return;
+    console.log(e);
+  } finally {
+    callback();
   }
-
-  const db = firebase.firestore();
-  const { createNode } = boundActionCreators;
-
-  const promises = types.map(
-    ({ collection, type, map = (node) => node }) =>
-      new Promise(async (resolve, reject) => {
-        try {
-          const snapshot = await db.collection(collection).get();
-
-          for (let doc of snapshot.docs) {
-            const contentDigest = getDigest(doc.id);
-
-            createNode(
-              Object.assign({}, map(doc.data()), {
-                id: doc.id,
-                parent: null,
-                children: [],
-                internal: {
-                  type,
-                  contentDigest,
-                },
-              })
-            );
-            resolve();
-          }
-        } catch (e) {
-          console.log(e);
-          resolve();
-        }
-      })
-  );
-
-  await Promise.all(promises);
-  return;
 };
