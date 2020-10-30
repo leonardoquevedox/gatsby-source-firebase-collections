48658a460c3331daeffb1be71b2a9d51452809aa
Leonardo Quevedo <leonardoq@trin.ca>
2020-10-30 10:43:55 -0300

    Adds bucket image fetching for image sharp usage.

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,18 @@ const isBucketImage = (value = '') =>
   value.indexOf('firebasestorage') > -1 &&
   getImageExtension(value);
 
+const getDigest = id =>
+  crypto
+    .createHash('md5')
+    .update(id)
+    .digest('hex');
+
 const transformPropertyOnMatch = (source, isMatch, transform) => {
+  if (!source || source === null)
+    return new Promise(resolve => {
+      resolve();
+    });
+
   return Promise.all(
     Object.keys(source).map(
       key =>
@@ -20,7 +31,7 @@ const transformPropertyOnMatch = (source, isMatch, transform) => {
           try {
             // eslint-disable-next-line
             if (isMatch(source[key])) {
-              source[key] = await transform(key, source[key]);
+              source = await transform(source, key, source[key]);
             } else if (typeof source[key] === 'object') {
               await transformPropertyOnMatch(source[key], isMatch, transform);
             }
@@ -33,52 +44,54 @@ const transformPropertyOnMatch = (source, isMatch, transform) => {
   );
 };
 
-const replaceImageUrlsWithFile = async (
+const createImageNodes = async (
   node,
-  { createNode, store, cache, createNodeId }
+  { createNode, store, cache, createNodeId, createParentChildLink }
 ) => {
   await transformPropertyOnMatch(
     node,
     isBucketImage,
-    (key, value) =>
+    (source, key, value) =>
       new Promise(async (resolve, reject) => {
-        const ext = getImageExtension(value);
-        console.log(ext);
-        // console.log('Creating remote file node for', value)
-        // For all MarkdownRemark nodes that have a featured image url, call createRemoteFileNode
-        let fileNode = await createRemoteFileNode({
-          url: value, // string that points to the URL of the image
-          parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
-          createNode, // helper function in gatsby-node to generate the node
-          createNodeId, // helper function in gatsby-node to generate the node id
-          cache, // Gatsby's cache
-          store, // Gatsby's redux store
-          ext, // Adds extension of picture,
-          name: Date.now(),
-        });
-        resolve(fileNode);
+        try {
+          const ext = getImageExtension(value);
+          const nameparts = value.replace(/%2F/g, '_').split('/');
+          const name = nameparts[nameparts.length - 1]
+            .split('?')[0]
+            .replace(ext, `.${ext}`);
+
+          // For all image nodes that have a featured image url, call createRemoteFileNode
+          let fileNode = await createRemoteFileNode({
+            url: value, // string that points to the URL of the image
+            parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
+            createNode, // helper function in gatsby-node to generate the node
+            createNodeId, // helper function in gatsby-node to generate the node id
+            cache, // Gatsby's cache
+            store, // Gatsby's redux store
+            name,
+          });
+
+          if (fileNode) {
+            fileNode.ext = ext;
+            fileNode.extension = ext;
+            fileNode.internal.mediaType = `image/${ext}`;
+
+            source[key.concat('___NODE')] = fileNode.id; // Creates foreign key
+            delete source[key]; // Prevents resolve duplicity errors
+
+            // source[key] = fileNode
+          }
+          resolve(source);
+        } catch (e) {
+          reject(e);
+        }
       })
   );
 };
 
-const getDigest = id =>
-  crypto
-    .createHash('md5')
-    .update(id)
-    .digest('hex');
-
-exports.createSchemaCustomization = ({ actions }, { types = [] }) => {
-  const { createTypes } = actions;
-  for (let i = 0; i < types.length; i++) {
-    const type = types[i];
-    const { definition } = type;
-    if (definition) createTypes(definition);
-  }
-};
-
 exports.sourceNodes = async (
   { boundActionCreators, createNodeId, store, cache },
-  { types = [], credential, appConfig },
+  { types = [], credential, appConfig, useBucketImagesSharp },
   callback
 ) => {
   try {
@@ -90,7 +103,7 @@ exports.sourceNodes = async (
     }
 
     const db = firebase.firestore();
-    const { createNode } = boundActionCreators;
+    const { createNode, createParentChildLink } = boundActionCreators;
 
     for (let i = 0; i < types.length; i++) {
       const entry = types[i];
@@ -104,31 +117,29 @@ exports.sourceNodes = async (
             const contentDigest = getDigest(doc.id);
             const values = { ...doc.data() };
 
-            await replaceImageUrlsWithFile(values, {
-              createNode,
-              store,
-              cache,
-              createNodeId,
-            });
-
-            console.log(JSON.stringify(values, null, 2));
-
-            console.log('\n Creating final node... \n');
-
-            createNode(
-              Object.assign(
-                {
-                  id: doc.id,
-                  parent: null,
-                  children: [],
-                  internal: {
-                    type,
-                    contentDigest,
-                  },
+            const node = Object.assign(
+              {
+                id: doc.id,
+                parent: null,
+                children: [],
+                internal: {
+                  type,
+                  contentDigest,
                 },
-                map(values)
-              )
+              },
+              map(values)
             );
+
+            if (useBucketImagesSharp)
+              await createImageNodes(node, {
+                createNode,
+                createNodeId,
+                createParentChildLink,
+                store,
+                cache,
+              });
+
+            createNode(node);
           }
         } else {
           createNode(
@@ -150,5 +161,14 @@ exports.sourceNodes = async (
     console.log(e);
   } finally {
     callback();
+  }
+};
+
+exports.createSchemaCustomization = ({ actions }, { types = [] }) => {
+  const { createTypes } = actions;
+  for (let i = 0; i < types.length; i++) {
+    const type = types[i];
+    const { definition } = type;
+    if (definition) createTypes(definition);
   }
 };
